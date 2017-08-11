3efde0c73161aec7f947a14500b87e89e34c62d3
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-11 20:27:05 -0300

    Commit inicial

---
+++ CalculaImposto/CalculadorDeImpostos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaImposto
+{
+    public class CalculadorDeImpostos
+    {
+        public void RealizaCalculo(Orcamento orcamento, IImposto imposto)
+        {
+            double icms = imposto.Calcula(orcamento);
+            Console.WriteLine(icms);
+        }
+    }
+}
---
+++ CalculaImposto/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaImposto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IImposto icms = new ICMS();
+            IImposto iss = new ISS();
+
+            Orcamento orcamento = new Orcamento(500.00);
+
+            CalculadorDeImpostos calculador = new CalculadorDeImpostos();
+
+            calculador.RealizaCalculo(orcamento, icms);
+            calculador.RealizaCalculo(orcamento, iss);
+
+            Console.ReadKey();
+        }
+    }
+}
