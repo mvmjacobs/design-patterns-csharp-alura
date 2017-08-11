f2dc869677cd4ce22bcfb592b37befeedeb2924a
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-11 20:48:36 -0300

    Classe para calculo do ICCC adicionada

---
+++ CalculaImposto/ICCC.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaImposto
+{
+    public class ICCC : IImposto
+    {
+        public double Calcula(Orcamento orcamento)
+        {
+            if (orcamento.Valor < 1000)
+                return orcamento.Valor * 0.05;
+
+            if (orcamento.Valor >= 1000 && orcamento.Valor <= 3000)
+                return orcamento.Valor * 0.07;
+
+            return orcamento.Valor * 0.08 + 30;
+        }
+    }
+}
