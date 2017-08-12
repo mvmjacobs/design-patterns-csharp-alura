2cd0613af343470bbb8b2200f68535a359cab6ae
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-11 21:14:00 -0300

    Criação da classe ContaBancaria

---
+++ CalculaInvestimento/ContaBancaria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaInvestimento
+{
+    public class ContaBancaria
+    {
+        public double Saldo { get; set; }
+
+        public ContaBancaria(double saldoInicial)
+        {
+            this.Saldo = saldoInicial;
+        }
+    }
+}
