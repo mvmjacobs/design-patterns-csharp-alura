9c30d13387cb4e469a3d320046b9b9d390304130
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-11 21:44:18 -0300

    Tipo de investimento Arrojado adicionado

---
+++ CalculaInvestimento/Arrojado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaInvestimento
+{
+    public class Arrojado : IInvestimento
+    {
+        public double Calcular(ContaBancaria conta)
+        {
+            int chance = new Random().Next(10);
+            if (chance >= 0 && chance <= 1)
+                return conta.Saldo * 0.05;
+
+            if (chance >= 2 && chance <= 4)
+                return conta.Saldo * 0.03;
+
+            return conta.Saldo * 0.006;
+        }
+    }
+}
