90ca496f48b9df27b56b148a631d0dd1437ab9c8
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-11 21:15:02 -0300

    Tipo de investimento Conservador adicionado

---
+++ CalculaInvestimento/Conservador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaInvestimento
+{
+    public class Conservador : IInvestimento
+    {
+        public void Investir(ContaBancaria conta)
+        {
+            conta.Saldo += conta.Saldo * 0.8;
+        }
+    }
+}
