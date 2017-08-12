ad5b1e0369741651b6fcbcf8929427a62cc8bd81
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-11 21:14:39 -0300

    Adicionado metodo para realizar o investimento

---
+++ CalculaInvestimento/RealizadorDeInvestimentos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaInvestimento
+{
+    public class RealizadorDeInvestimentos
+    {
+        public void RealizaInvestimento(ContaBancaria conta, IInvestimento investimento)
+        {
+            investimento.Investir(conta);
+            Console.WriteLine(conta.Saldo);
+        }
+    }
+}
