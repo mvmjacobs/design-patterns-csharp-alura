711f1487a63f458fda6c9ad15f233cc428c77ffb
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-11 21:23:34 -0300

    Tipo de investimento moderado adicionado

---
+++ CalculaInvestimento/Moderado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaInvestimento
+{
+    public class Moderado : IInvestimento
+    {
+        public void Investir(ContaBancaria conta)
+        {
+            double valor = conta.Saldo * (new Random().Next(101) > 50 ? 0.025 : 0.007);
+            conta.Saldo += valor;
+        }
+    }
+}
