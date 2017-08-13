80152ea7c71a588ec449bf2ebe3ba352e1ee23ae
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-12 23:08:01 -0300

    Correção do retorno da resposta em XML

--- RequisicaoConta/RespostaEmXML.cs
+++ RequisicaoConta/RespostaEmXML.cs
@@ -13,7 +13,7 @@ namespace RequisicaoConta
         public void Responder(Conta conta, EFormato formato)
         {
             if (formato == EFormato.XML)
-                Console.WriteLine(String.Format("<conta><titular>{0}</titular><saldo>{1}</saldo></conta>"));
+                Console.WriteLine(String.Format("<conta><titular>{0}</titular><saldo>{1}</saldo></conta>", conta.Titular, conta.Saldo));
             else
                 OutraResposta.Responder(conta, formato);
 ;       }
