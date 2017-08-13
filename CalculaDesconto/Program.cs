0795a27e929cdb24fd8c2b62c5c55551df57a504
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-12 22:05:41 -0300

    Visualização do desconto adicionada ao Main

--- CalculaDesconto/Program.cs
+++ CalculaDesconto/Program.cs
@@ -10,6 +10,18 @@ namespace CalculaDesconto
     {
         static void Main(string[] args)
         {
+            Orcamento orcamento = new Orcamento(0);
+            orcamento.AdicionarItem(new Item("Teclado", 100));
+            orcamento.AdicionarItem(new Item("Mouse", 100));
+            orcamento.AdicionarItem(new Item("Monitor", 1000));
+            orcamento.AdicionarItem(new Item("Placa de video", 1500));
+            orcamento.AdicionarItem(new Item("HD", 500));
+
+            CalculadorDeDesconto calculador = new CalculadorDeDesconto();
+            double desconto = calculador.CalcularDesconto(orcamento);
+            Console.WriteLine(desconto);
+
+            Console.ReadKey();
         }
     }
 }
