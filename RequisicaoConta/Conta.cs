3ab0384e8b8a826c440d75278f604ad93cfe1bd6
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-12 22:43:55 -0300

    Criação da classe conta

---
+++ RequisicaoConta/Conta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequisicaoConta
+{
+    public class Conta
+    {
+        public string Titular { get; set; }
+        public double Saldo { get; set; }
+
+        public Conta(string titular, double saldo)
+        {
+            this.Titular = titular;
+            this.Saldo = saldo;
+        }
+    }
+}
