2c8b9afd4f3666e540fa26136cad0d17d253c88c
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-12 21:59:35 -0300

    Adicionado ultimo desconto ao calculador

--- CalculaDesconto/CalculadorDeDesconto.cs
+++ CalculaDesconto/CalculadorDeDesconto.cs
@@ -12,8 +12,10 @@ namespace CalculaDesconto
         {
             IDesconto d1 = new DescontoMaisDeCincoItens();
             IDesconto d2 = new DescontoValorMaiorQue500();
+            IDesconto ultimo = new SemDesconto();
 
             d1.Proximo = d2;
+            d2.Proximo = ultimo;
 
             return d1.Calcular(orcamento);
         }
