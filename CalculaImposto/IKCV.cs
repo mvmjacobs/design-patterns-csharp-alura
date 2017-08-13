b48eb18e85df64791fc5f237ebf0d4d82ffdf149
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-12 23:53:56 -0300

    Ciração do imposto IKCV

---
+++ CalculaImposto/IKCV.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaImposto
+{
+    public class IKCV : ImpostoTemplate
+    {
+        public override double MaximaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.1;
+        }
+
+        public override double MinimaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.06;
+        }
+
+        public override bool UsaMaximaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.Valor > 500 && TemItemComValorSuperiorA100(orcamento);
+        }
+
+        private bool TemItemComValorSuperiorA100(Orcamento orcamento)
+        {
+            foreach (var item in orcamento.Itens)
+            {
+                if (item.Valor > 100)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
