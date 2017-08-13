5a8942de628e5c3b1dd4f2bd4bbbc609d096d283
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-12 21:41:33 -0300

    Criação da classe SemDesconto

---
+++ CalculaDesconto/SemDesconto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaDesconto
+{
+    public class SemDesconto : IDesconto
+    {
+        public IDesconto Proximo { get; set; }
+
+        public double Calcular(Orcamento orcamento)
+        {
+            return 0;
+        }
+    }
+}
