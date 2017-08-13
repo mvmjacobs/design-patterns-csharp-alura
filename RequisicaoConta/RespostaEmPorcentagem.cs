faf4f53d54ecf15f88f164030581e0d2f40a0601
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-12 23:18:55 -0300

    Criação da classe para resposta com porcento

---
+++ RequisicaoConta/RespostaEmPorcentagem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequisicaoConta
+{
+    public class RespostaEmPorcentagem : IResposta
+    {
+        public IResposta OutraResposta { get; set; }
+
+        public void Responder(Conta conta, EFormato formato)
+        {
+            if (formato == EFormato.PORCENTO)
+                Console.WriteLine(String.Format("{0}%{1}", conta.Titular, conta.Saldo));
+            else
+                OutraResposta.Responder(conta, formato);
+        }
+    }
+}
