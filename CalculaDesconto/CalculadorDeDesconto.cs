2832c06cfcfe47221880ffd239e9ae12e5af6f18
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-12 21:36:50 -0300

    Criação da classe para calcular o desconto

---
+++ CalculaDesconto/CalculadorDeDesconto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaDesconto
+{
+    public class CalculadorDeDesconto
+    {
+        public double CalcularDesconto(Orcamento orcamento)
+        {
+            IDesconto d1 = new DescontoMaisDeCincoItens();
+            IDesconto d2 = new DescontoValorMaiorQue500();
+
+            d1.Proximo = d2;
+
+            return d1.Calcular(orcamento);
+        }
+    }
+}
