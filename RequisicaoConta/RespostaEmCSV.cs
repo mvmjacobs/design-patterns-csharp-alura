54f3c9ee1a68ed9061f553d8fc0a291eacd481b3
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-12 23:07:30 -0300

    Criação da classe para resposta em CSV

---
+++ RequisicaoConta/RespostaEmCSV.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequisicaoConta
+{
+    public class RespostaEmCSV : IResposta
+    {
+        public IResposta OutraResposta { get; set; }
+
+        public void Responder(Conta conta, EFormato formato)
+        {
+            if (formato == EFormato.CSV)
+                Console.WriteLine(String.Format("{0};{1}", conta.Titular, conta.Saldo));
+            else
+                OutraResposta.Responder(conta, formato);
+        }
+    }
+}
