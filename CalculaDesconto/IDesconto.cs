e583a70e67231b7ff8c60256d1e6e141f517e437
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-12 21:21:02 -0300

    Criação da interface de Desconto

---
+++ CalculaDesconto/IDesconto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaDesconto
+{
+    public interface IDesconto
+    {
+        IDesconto Proximo { get; set; }
+        double Calcular(Orcamento orcamento);
+    }
+}
