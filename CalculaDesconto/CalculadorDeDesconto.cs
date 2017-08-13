12a1cbaa955338ff93401c4f60b59442416d884d
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-12 22:18:18 -0300

    Adicionado desconto de venda casa no calculador de desconto

--- CalculaDesconto/CalculadorDeDesconto.cs
+++ CalculaDesconto/CalculadorDeDesconto.cs
@@ -12,10 +12,12 @@ namespace CalculaDesconto
         {
             IDesconto d1 = new DescontoMaisDeCincoItens();
             IDesconto d2 = new DescontoValorMaiorQue500();
+            IDesconto d3 = new DescontoPorVendaCasada();
             IDesconto ultimo = new SemDesconto();
 
             d1.Proximo = d2;
-            d2.Proximo = ultimo;
+            d2.Proximo = d3;
+            d3.Proximo = ultimo;
 
             return d1.Calcular(orcamento);
         }
