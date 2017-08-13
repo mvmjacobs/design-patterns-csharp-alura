ffc1228fc197e5cde9ad642c5b1a85e6641a6766
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-12 22:17:17 -0300

    Criação do desconto para venda casada

---
+++ CalculaDesconto/DescontoPorVendaCasada.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaDesconto
+{
+    public class DescontoPorVendaCasada : IDesconto
+    {
+        public IDesconto Proximo { get; set; }
+
+        public double Calcular(Orcamento orcamento)
+        {
+            if (Existe(orcamento, "LAPIS") && Existe(orcamento, "CANETA"))
+                return orcamento.Valor * 0.05;
+
+            return Proximo.Calcular(orcamento);
+        }
+
+        private bool Existe(Orcamento orcamento, string nomeDoItem)
+        {
+            return orcamento.Itens.FirstOrDefault(i => i.Nome == nomeDoItem) != null;
+        }
+    }
+}
