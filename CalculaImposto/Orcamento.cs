62f6e24f0d65f6782a3ab69ab802b2f01b96ad7f
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-12 23:53:33 -0300

    Propriedade Itens adicionada ao orcamento

--- CalculaImposto/Orcamento.cs
+++ CalculaImposto/Orcamento.cs
@@ -9,10 +9,18 @@ namespace CalculaImposto
     public class Orcamento
     {
         public double Valor { get; private set; }
+        public IList<Item> Itens { get; private set; }
 
         public Orcamento(double valor)
         {
             this.Valor = valor;
+            this.Itens = new List<Item>();
+        }
+
+        public void AdicionarItem(Item item)
+        {
+            this.Itens.Add(item);
+            this.Valor += item.Valor;
         }
     }
 }
