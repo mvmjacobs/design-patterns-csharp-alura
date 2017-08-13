8048d9d8e3e15cbf4aed593075f903f13b17b488
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-12 22:45:25 -0300

    Criação do enum para formato

---
+++ RequisicaoConta/EFormato.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequisicaoConta
+{
+    public enum EFormato
+    {
+        XML,
+        CSV,
+        PORCENTO
+    }
+}
