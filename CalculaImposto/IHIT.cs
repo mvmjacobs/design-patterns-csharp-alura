594bf9e6cca864094c2a9b04fa56fca7e7d5301f
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-13 00:00:44 -0300

    Criação do imposto IHIT

---
+++ CalculaImposto/IHIT.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaImposto
+{
+    public class IHIT : ImpostoTemplate
+    {
+        public override double MaximaTaxacao(Orcamento orcamento)
+        {
+            return (orcamento.Valor * 0.13) + 100;
+        }
+
+        public override double MinimaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.Valor * (0.01 * orcamento.Itens.Count);
+        }
+
+        public override bool UsaMaximaTaxacao(Orcamento orcamento)
+        {
+            IList<string> lst = new List<string>();
+            foreach(var item in orcamento.Itens)
+            {
+                if (lst.Contains(item.Nome))
+                    return true;
+                lst.Add(item.Nome);
+            }
+            return false;
+        }
+    }
+}
