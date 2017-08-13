2bea8bdec7155d2983e9917c6345eb03629d2351
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-12 22:49:07 -0300

    Criação da interface para resposta

---
+++ RequisicaoConta/IResposta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequisicaoConta
+{
+    interface IResposta
+    {
+        IResposta Proxima { get; set; }
+        void Responder(Conta conta, EFormato formato);
+    }
+}
