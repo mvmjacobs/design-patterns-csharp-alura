48ee7868654cb2fa35eeeaf6d165f6d3edaff8c9
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-12 21:27:48 -0300

    Lista de Item adicionada ao orçamento

--- CalculaDesconto/Orcamento.cs
+++ CalculaDesconto/Orcamento.cs
@@ -9,10 +9,17 @@ namespace CalculaDesconto
     public class Orcamento
     {
         public double Valor { get; private set; }
+        public IList<Item> Itens { get; private set; }
 
         public Orcamento(double valor)
         {
             this.Valor = valor;
+            this.Itens = new List<Item>();
+        }
+
+        public void AdicionarItem(Item item)
+        {
+            this.Itens.Add(item);
         }
     }
 }
