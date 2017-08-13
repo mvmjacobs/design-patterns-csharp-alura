05b08abf3731a4cdc797e95d6cd166b97ffe8ca8
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-12 23:47:23 -0300

    Criação do tipo de imposto ICPP

---
+++ CalculaImposto/ICPP.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaImposto
+{
+    public class ICPP : ImpostoTemplate
+    {
+        public override double MaximaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.07;
+        }
+
+        public override double MinimaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.05;
+        }
+
+        public override bool UsaMaximaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.Valor >= 500;
+        }
+    }
+}
