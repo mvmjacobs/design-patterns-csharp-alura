8fafb024d954ace23e27f9eca29e70fa447f5aac
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-12 23:03:56 -0300

    Criação da resposta em XML

---
+++ RequisicaoConta/RespostaEmXML.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequisicaoConta
+{
+    public class RespostaEmXML : IResposta
+    {
+        public IResposta Proxima { get; set; }
+
+        public void Responder(Conta conta, EFormato formato)
+        {
+            if (formato == EFormato.XML)
+                Console.WriteLine(String.Format("<conta><titular>{0}</titular><saldo>{1}</saldo></conta>"));
+            else
+                Proxima.Responder(conta, formato);
+;       }
+    }
+}
