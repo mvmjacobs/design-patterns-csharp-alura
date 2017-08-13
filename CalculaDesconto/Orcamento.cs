de0160e07302e25d84b0ee9d9c77aa2e9185aa33
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-12 21:20:45 -0300

    Criação da classe Orcamento

---
+++ CalculaDesconto/Orcamento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaDesconto
+{
+    public class Orcamento
+    {
+        public double Valor { get; private set; }
+
+        public Orcamento(double valor)
+        {
+            this.Valor = valor;
+        }
+    }
+}
