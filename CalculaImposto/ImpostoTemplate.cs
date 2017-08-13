b51e7aeaff7acde985a1b8585a93ef2e0ca15616
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-12 23:44:52 -0300

    Criação do template method para calculo de imposto

---
+++ CalculaImposto/ImpostoTemplate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaImposto
+{
+    public abstract class ImpostoTemplate : IImposto
+    {
+        public abstract bool UsaMaximaTaxacao(Orcamento orcamento);
+        public abstract double MaximaTaxacao(Orcamento orcamento);
+        public abstract double MinimaTaxacao(Orcamento orcamento);
+
+        public double Calcula(Orcamento orcamento)
+        {
+            if (UsaMaximaTaxacao(orcamento))
+                return MaximaTaxacao(orcamento);
+
+            return MinimaTaxacao(orcamento);
+        }
+    }
+}
