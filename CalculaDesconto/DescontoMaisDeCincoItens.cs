09b7c43568b1b481a44d0adf6c86af884917ea77
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-12 21:23:46 -0300

    Criação da classe de desconto para mais de 5 itens

---
+++ CalculaDesconto/DescontoMaisDeCincoItens.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaDesconto
+{
+    public class DescontoMaisDeCincoItens : IDesconto
+    {
+        public IDesconto Proximo { get; set; }
+
+        public double Calcular(Orcamento orcamento)
+        {
+            // TODO: Aplicar a regra
+            return 0;
+        }
+    }
+}
