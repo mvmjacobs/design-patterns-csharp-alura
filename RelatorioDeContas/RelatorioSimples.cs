d63386a1637ff8ada30082c8d279b5a5493d1708
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-13 21:52:41 -0300

    Criação do modelo de relatorio simples

---
+++ RelatorioDeContas/RelatorioSimples.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelatorioDeContas
+{
+    public class RelatorioSimples : RelatorioTemplate
+    {
+        public override void Cabecalho()
+        {
+            Console.WriteLine("Caixa Economica Federal");
+        }
+
+        public override void Principal(IList<Conta> contas)
+        {
+            foreach (var conta in contas)
+            {
+                Console.WriteLine(String.Format("{0} - {1}", conta.Titular, conta.Saldo));
+            }
+        }
+
+        public override void Rodape()
+        {
+            Console.WriteLine("+11 (11) 1111-1111");
+        }
+    }
+}
