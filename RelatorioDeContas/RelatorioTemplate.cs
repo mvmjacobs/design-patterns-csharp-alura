53c8277e5ed06f21df8ba2638be31ce76889e5b1
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-13 21:42:37 -0300

    Criação do template para relatorio

---
+++ RelatorioDeContas/RelatorioTemplate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelatorioDeContas
+{
+    public abstract class RelatorioTemplate
+    {
+        public abstract void Cabecalho();
+        public abstract void Rodape();
+        public abstract void Principal(IList<Conta> contas);
+
+        public void Imprime(IList<Conta> contas)
+        {
+            Cabecalho();
+            Principal(contas);
+            Rodape();
+        }
+    }
+}
