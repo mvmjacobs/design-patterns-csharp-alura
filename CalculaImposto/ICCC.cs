80145a05388d211561914d97556eb443636e84ab
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-13 22:28:54 -0300

    Adicionada a implementação do OutroImposto para o imposto ICCC

--- CalculaImposto/ICCC.cs
+++ CalculaImposto/ICCC.cs
@@ -6,17 +6,21 @@ using System.Threading.Tasks;
 
 namespace CalculaImposto
 {
-    public class ICCC : IImposto
+    public class ICCC : Imposto
     {
-        public double Calcula(Orcamento orcamento)
+        public ICCC(IImposto outroImposto) : base(outroImposto) { }
+        public ICCC() : base() { }
+
+        public override double Calcula(Orcamento orcamento)
         {
+            double valorDoOutroImposto = CalculoDoOutroImposto(orcamento);
             if (orcamento.Valor < 1000)
-                return orcamento.Valor * 0.05;
+                return orcamento.Valor * 0.05 + valorDoOutroImposto;
 
             if (orcamento.Valor >= 1000 && orcamento.Valor <= 3000)
-                return orcamento.Valor * 0.07;
+                return orcamento.Valor * 0.07 + valorDoOutroImposto;
 
-            return orcamento.Valor * 0.08 + 30;
+            return orcamento.Valor * 0.08 + 30 + valorDoOutroImposto;
         }
     }
 }
