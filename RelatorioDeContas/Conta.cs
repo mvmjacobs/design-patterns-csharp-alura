9321a4ad30aa3dba315703eb8382e06b36effd29
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-13 21:37:23 -0300

    Criação da classe conta

---
+++ RelatorioDeContas/Conta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelatorioDeContas
+{
+    public class Conta
+    {
+        public string Titular { get; private set; }
+        public string Agencia { get; private set; }
+        public string Numero { get; private set; }
+        public double Saldo { get; private set; }
+
+        public Conta(string titular, string agencia, string numero)
+        {
+            this.Titular = titular;
+            this.Agencia = agencia;
+            this.Numero = numero;
+        }
+
+        public void Depositar(double valor)
+        {
+            this.Saldo += valor;
+        }
+    }
+}
