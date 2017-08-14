05b36691db2c44fadb7cdee4825bd531f9f45d82
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-08-13 22:19:51 -0300

    Criação da classe abstrata de imposto

---
+++ CalculaImposto/Imposto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaImposto
+{
+    public abstract class Imposto : IImposto
+    {
+        public double Calcula(Orcamento orcamento)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
